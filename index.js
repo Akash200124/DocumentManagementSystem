b68568cfc298b5acb1786b086aa339f902b0b469
akash <akashkuamr200124@gmail.com>
2025-02-07 01:03:40 +0530

    change in index file

---
+++ index.js
@@ -0,0 +1,27 @@
+import { app } from './app.js';
+import connecttoDB from './src/db/db.js';
+import dotenv from 'dotenv';
+
+
+
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+dotenv.config(
+  {
+      path: './.env'
+  }
+);
+
+//connect to mongo db 
+connecttoDB().then(() => {
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+}).catch((err) => console.log("error in db connection", err))
