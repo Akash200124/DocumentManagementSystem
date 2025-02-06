26dd1d075f19e1187bb378ad7c898bb66bd12266
akash <akashkuamr200124@gmail.com>
2025-02-07 01:27:30 +0530

    cros fixing

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ import express from "express";
 
 const app =express();
 
+
+const corsOptions = {
+    origin: "https://document-management-system-fe.vercel.app",
+}
+
+app.use(cors(corsOptions));
+
 app.use(express.json({ "limit": "16kb" }));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
